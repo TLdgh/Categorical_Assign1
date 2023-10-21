0b01b9e5b66bd619493da63da8198b59fb9a27dd
TLdgh <teng.li2@mail.mcgill.ca>
2023-10-21 16:49:59 -0400

    updated IndepTest function

--- CodeSpace.R
+++ CodeSpace.R
@@ -7,20 +7,31 @@ oddsratio<-function(tbl,row.names=NULL){
 
 IndepTest<-setRefClass(
   "IndepTest",
-  fields = list(data="data.frame", variable_x="character", variable_y="character",res="list"),
+  fields = list(data="data.frame", variable_x="character", reference_y="character",pval_df="data.frame",ContingencyTbl="list" ),
   methods=list(
-    initialize=function(data, variable_x, variable_y){
+    initialize=function(data, variable_x, reference_y){
       #make a contingency table for the variable X and Y
-      for(x in variable_x){
-        .self$res[[x]]<-data%>%select(all_of(c(x, variable_y)))%>%na.omit()%>%
-          group_by(across(everything(), as.factor))%>%tally()%>%spread(key=variable_y, value=n)}
+      .self$ContingencyTbl<-.self$create_list(data, variable_x, reference_y)
+      
+      #calculate the pvalues and make a data frame
+      .self$pval_df<- .self$chi_square_test(my_list=ContingencyTbl)
     },
     
-    get_pvalue=function(){
+    create_list=function(data, variable_x, reference_y){
+      my_list <- list()
+      for(x in variable_x){
+        my_list[[x]]<-data%>%select(all_of(c(x, reference_y)))%>%na.omit()%>%
+          group_by(across(everything(), as.factor))%>%tally()%>%spread(key=reference_y, value=n)}
+      return(my_list)
+    }, 
+    
+    chi_square_test=function(my_list){
       #do the chi-squared test
-      pvalues<-map(res, function(x){
-        y<-ungroup(x)%>%select(-any_of(group_vars(x)))%>%as.matrix()%>%chisq.test()
-        return(y$p.value)
+      pvalues<-map(my_list, function(x){
+        if(any(is.na(x))==FALSE){
+          y<-ungroup(x)%>%select(-any_of(group_vars(x)))%>%as.matrix()%>%chisq.test()
+          return(y$p.value)
+        }else{return("Has no values in Contingency Table")}
       })
       
       #format it nicely
